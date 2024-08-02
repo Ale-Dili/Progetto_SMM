dc59ee3266a764424f0e49618219d0a809771757
Alessandro Di Liberti <ale.diliberti@gmail.com>
2024-08-02 17:46:52 +0200

    first commit

---
+++ node.py
@@ -0,0 +1,5 @@
+class Node:
+    data = 2 
+        
+    def say_hello(self):
+        print(f'Hello, I am a node with data {self.data}')
---
+++ tree_predictor.py
@@ -0,0 +1,6 @@
+from ucimlrepo import fetch_ucirepo 
+import node
+
+node1 = node.Node()
+
+print(node1.data)
