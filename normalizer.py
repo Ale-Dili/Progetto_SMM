53e1051de6457d96c7174d435ef4d38878122813
Alessandro Di Liberti <ale.diliberti@gmail.com>
2024-08-04 16:04:29 +0200

    add label fixer

--- normalizer.py
+++ normalizer.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 class Normalizer:
-    def __init__(self, n_bins=4, normalization="z-score"):
+    def __init__(self, n_bins=4, normalization="z-score", encoding=None):
         self.n_bins = n_bins
         if normalization in ["z-score", "minmax"]:
             self.normalization = normalization
+        else:
+            self.normalization = None
+        if encoding in ['onehot']:
+            self.encoding = encoding
+        else:
+            self.encoding = None
 
     def normalize(self, X):
         for colname in X.columns:
@@ -16,9 +22,10 @@ class Normalizer:
 
     # One hot encoding
     def _categorical_norm(self, X, colname):
-        dummies = pd.get_dummies(X[colname], prefix=colname)
-        X = pd.concat([X, dummies], axis=1)
-        X.drop(colname, axis=1, inplace=True)
+        if self.encoding:
+            dummies = pd.get_dummies(X[colname], prefix=colname)
+            X = pd.concat([X, dummies], axis=1)
+            X.drop(colname, axis=1, inplace=True)
         return X
 
     # Z-score or minmax normalization
@@ -30,3 +37,8 @@ class Normalizer:
         elif self.normalization == 'minmax':
             column = (column - column.min()) / (column.max() - column.min())
         return column
+
+    #fix label 
+    def _fix_label(self, y):
+        y['class'] = y['class'].apply(lambda x: 1 if x == 'e' else 0)
+        return y
